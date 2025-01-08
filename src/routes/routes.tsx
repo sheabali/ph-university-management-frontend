f487ac3146337b47330d36666ce26e0088357d4b
sheabali <iamskpranto@gmail.com>
2025-01-08 23:23:45 +0600

    create CreateAdmin

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -38,6 +38,10 @@ const routes = createBrowserRouter([
         path: 'create-student',
         element: <CreateStudent />,
       },
+      {
+        path: 'create-faculty',
+        element: <CreateStudent />,
+      },
     ],
   },
   {
