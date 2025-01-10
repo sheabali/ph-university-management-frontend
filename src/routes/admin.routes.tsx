9a7352817fedd7e456201367be8c6d160ed66e32
sheabali <iamskpranto@gmail.com>
2025-01-10 18:56:53 +0600

    create a utils file witch name route Generator

--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -5,11 +5,6 @@ import CreateFaculty from '../pages/admin/CreateFaculty';
 import CreateStudent from '../pages/admin/CreateStudent';
 import { NavLink } from 'react-router-dom';
 
-type TRoute = {
-  path: string;
-  element: ReactNode;
-};
-
 type TSidebarItem = {
   key: string;
   label: ReactNode;
@@ -51,25 +46,25 @@ const adminPaths = [
   },
 ];
 
-export const adminRoutes = adminPaths.reduce((acc: TRoute[], item) => {
-  if (item.path && item.element) {
-    acc.push({
-      path: item.path,
-      element: item.element,
-    });
-  }
+// export const adminRoutes = adminPaths.reduce((acc: TRoute[], item) => {
+//   if (item.path && item.element) {
+//     acc.push({
+//       path: item.path,
+//       element: item.element,
+//     });
+//   }
 
-  if (item.children) {
-    item.children.forEach((child) => {
-      acc.push({
-        path: child.path,
-        element: child.element,
-      });
-    });
-  }
+//   if (item.children) {
+//     item.children.forEach((child) => {
+//       acc.push({
+//         path: child.path,
+//         element: child.element,
+//       });
+//     });
+//   }
 
-  return acc;
-}, []);
+//   return acc;
+// }, []);
 
 export const adminSidebarItem = adminPaths.reduce(
   (acc: TSidebarItem[], item) => {
