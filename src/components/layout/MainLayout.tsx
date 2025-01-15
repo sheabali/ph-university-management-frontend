55fa5f69312f8325018917a70cb3627c6f566b2d
sheabali <iamskpranto@gmail.com>
2025-01-15 19:44:37 +0600

    logout button working

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,38 +1,25 @@
-import { Layout } from 'antd';
+import { Button, Layout } from 'antd';
 import { Outlet } from 'react-router-dom';
 import Sidebar from './Sidebar';
 import { Content, Header } from 'antd/es/layout/layout';
+import { useAppDispatch } from '../../redux/hooks';
+import { logout } from '../../redux/features/auth/authSlice';
 
-// const items: MenuProps['items'] = [
-//   {
-//     key: 'Dashboard',
-//     label: <NavLink to="/admin/dashboard">Dashboard</NavLink>,
-//   },
-//   {
-//     key: 'User Management',
-//     label: 'User Management',
-//     children: [
-//       {
-//         key: 'Create Admin',
-//         label: <NavLink to="/admin/create-admin">Create Admin </NavLink>,
-//       },
-//       {
-//         key: 'Create Faculty',
-//         label: <NavLink to="/admin/create-faculty">Create Faculty </NavLink>,
-//       },
-//       {
-//         key: 'Create Student',
-//         label: <NavLink to="/admin/create-student">Create Student </NavLink>,
-//       },
-//     ],
-//   },
-// ];
 const MainLayout = () => {
+  const dispatch = useAppDispatch();
+
+  const handleSubmit = () => {
+    dispatch(logout());
+  };
+
   return (
     <Layout style={{ height: '100vh' }}>
+      <Sidebar />
       <Layout>
-        <Sidebar />
-        <Header style={{ padding: 0 }} />
+        <Header>
+          <Button onClick={() => handleSubmit()}>Logout</Button>
+        </Header>
+
         <Content style={{ margin: '24px 16px 0' }}>
           <div
             style={{
--- src/redux/features/auth/authSlice.ts
+++ src/redux/features/auth/authSlice.ts
@@ -27,7 +27,7 @@ const authSlice = createSlice({
   },
 });
 
-export const { setUser } = authSlice.actions;
+export const { setUser, logout } = authSlice.actions;
 
 export default authSlice.reducer;
 
