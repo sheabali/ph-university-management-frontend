fa9c85af176fb8cef5599935f32b5a0356dbbe6c
sheabali <iamskpranto@gmail.com>
2025-01-15 14:42:12 +0600

    Protected Route, overview,

--- src/components/layout/ProtectedRoute.tsx
+++ src/components/layout/ProtectedRoute.tsx
@@ -1,6 +1,15 @@
 import { ReactNode } from 'react';
+import { useCurrentToken } from '../../redux/features/auth/authSlice';
+import { useAppSelector } from '../../redux/hooks';
+import { Navigate } from 'react-router-dom';
 
 const ProtectedRoute = ({ children }: { children: ReactNode }) => {
+  const token = useAppSelector(useCurrentToken);
+  console.log(token);
+  if (!token) {
+    return <Navigate to="/login" replace={true} />;
+  }
+
   return children;
 };
 
