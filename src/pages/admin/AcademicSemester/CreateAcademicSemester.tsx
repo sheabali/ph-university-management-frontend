b17c1a7d0a23cde4b496336ba3570941ac8efafb
sheabali <iamskpranto@gmail.com>
2025-01-19 16:24:11 +0600

    Reusable select element

--- src/pages/admin/AcademicSemester/CreateAcademicSemester.tsx
+++ src/pages/admin/AcademicSemester/CreateAcademicSemester.tsx
@@ -1,18 +1,43 @@
 import { FieldValues, SubmitHandler } from 'react-hook-form';
 import PHForm from '../../../components/form/PHForm';
-import PHInput from '../../../components/form/PHInput';
-import { Button } from 'antd';
 
+import { Button, Col, Flex } from 'antd';
+import PHSelect from '../../../components/form/PHSelect';
+const nameOptions = [
+  {
+    value: '01',
+    label: 'Autuam',
+  },
+  {
+    value: '02',
+    label: 'Summer',
+  },
+  {
+    value: '03',
+    label: 'Fall',
+  },
+];
 const CreateAcademicSemester = () => {
+  const semesterData = {
+    name: 'Something',
+    code: 'Something',
+  };
+  console.log(semesterData);
+
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
     console.log(data);
   };
 
   return (
-    <PHForm onSubmit={onSubmit}>
-      <PHInput type="text" name="name" />
-      <Button htmlType="submit">Submit</Button>
-    </PHForm>
+    <Flex justify="center" align="center">
+      <Col span={10}>
+        <PHForm onSubmit={onSubmit}>
+          <PHSelect options={nameOptions} label="Name" name="name" />
+
+          <Button htmlType="submit">Submit</Button>
+        </PHForm>
+      </Col>
+    </Flex>
   );
 };
 
