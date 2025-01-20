89243f9509aed01d027aaa4e702d9fa2a334ef5f
sheabali <iamskpranto@gmail.com>
2025-01-20 15:48:21 +0600

    Generic type for response data and query params recap

--- src/pages/admin/AcademicSemester/AcademicSemester.tsx
+++ src/pages/admin/AcademicSemester/AcademicSemester.tsx
@@ -8,7 +8,9 @@ type TTbaleData = Pick<
 >;
 
 const AcademicSemester = () => {
-  const { data: semesterData } = useGetAllSemesterQuery(undefined);
+  const { data: semesterData } = useGetAllSemesterQuery([
+    { name: 'year', value: '2027' },
+  ]);
   console.log(semesterData);
   const tableData = semesterData?.data?.map(
     ({ _id, name, year, startMonth, endMonth }) => ({
@@ -25,6 +27,35 @@ const AcademicSemester = () => {
       title: 'Name',
       dataIndex: 'name',
       showSorterTooltip: { target: 'full-header' },
+      filters: [
+        {
+          text: 'Joe',
+          value: 'Joe',
+        },
+        {
+          text: 'Jim',
+          value: 'Jim',
+        },
+        {
+          text: 'Submenu',
+          value: 'Submenu',
+          children: [
+            {
+              text: 'Green',
+              value: 'Green',
+            },
+            {
+              text: 'Black',
+              value: 'Black',
+            },
+          ],
+        },
+      ],
+      // specify the condition of filtering result
+      // here is that finding the name started with `value`
+      onFilter: (value, record) => record.name.indexOf(value as string) === 0,
+      sorter: (a, b) => a.name.length - b.name.length,
+      sortDirections: ['descend'],
     },
     {
       title: 'Year',
