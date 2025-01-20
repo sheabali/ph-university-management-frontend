cadbd83415404ada3d4d454738fce8a132ad7f38
sheabali <iamskpranto@gmail.com>
2025-01-20 13:49:36 +0600

    Show semester data using Ant Design Table

--- src/pages/admin/AcademicSemester/AcademicSemester.tsx
+++ src/pages/admin/AcademicSemester/AcademicSemester.tsx
@@ -1,10 +1,63 @@
+import { Table, TableColumnsType, TableProps } from 'antd';
 import { useGetAllSemesterQuery } from '../../../redux/features/admin/academicManagement.api';
 
-const AcademicSemester = () => {
-  const { data } = useGetAllSemesterQuery(undefined);
-  console.log(data);
+interface DataType {
+  key: React.Key;
+  name: string;
+  age: number;
+  address: string;
+}
 
-  return <div>AcademicSemester</div>;
+const AcademicSemester = () => {
+  const { data: semesterData } = useGetAllSemesterQuery(undefined);
+
+  const tableData = semesterData?.data.map(
+    ({ _id, name, year, startMonth, endMonth }) => ({
+      _id,
+      name,
+      year,
+      startMonth,
+      endMonth,
+    })
+  );
+
+  const columns: TableColumnsType<DataType> = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      showSorterTooltip: { target: 'full-header' },
+    },
+    {
+      title: 'Year',
+      dataIndex: 'year',
+    },
+    {
+      title: 'Start Month',
+      dataIndex: 'startMonth',
+    },
+    {
+      title: 'End Month',
+      dataIndex: 'endMonth',
+    },
+  ];
+
+  const onChange: TableProps<DataType>['onChange'] = (
+    pagination,
+    filters,
+    sorter,
+    extra
+  ) => {
+    console.log('params', pagination, filters, sorter, extra);
+  };
+
+  return (
+    <Table<DataType>
+      columns={columns}
+      dataSource={tableData}
+      onChange={onChange}
+      showSorterTooltip={{ target: 'sorter-icon' }}
+    />
+  );
 };
 
 export default AcademicSemester;
