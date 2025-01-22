33ff7ecb0b3127156258595d24c0e1a7099db93d
sheabali <iamskpranto@gmail.com>
2025-01-23 03:07:08 +0600

    Refactor table date and handle status change using antd dropdown

--- src/pages/admin/courseManagement/RegisteredSemesters.tsx
+++ src/pages/admin/courseManagement/RegisteredSemesters.tsx
@@ -1,13 +1,27 @@
-import { Button, Table, TableColumnsType } from 'antd';
+import { Button, Dropdown, Table, TableColumnsType, Tag } from 'antd';
 
 import { TSemester } from '../../../types';
 import { useGetAllRegisteredSemestersQuery } from '../../../redux/features/admin/courseManagement';
+import moment from 'moment';
 
 type TTbaleData = Pick<TSemester, 'startDate' | 'endDate' | 'status'>;
 
-const RegisteredSemesters = () => {
-  // const [params, setParams] = useState<TQueryParams[] | undefined>(undefined);
+const items = [
+  {
+    label: 'Opcoming',
+    key: 'OPCOMING',
+  },
+  {
+    label: 'Ongoing',
+    key: 'ONGOING',
+  },
+  {
+    label: 'Ended',
+    key: 'ENDED',
+  },
+];
 
+const RegisteredSemesters = () => {
   const { data: semesterData, isFetching } =
     useGetAllRegisteredSemestersQuery(undefined);
 
@@ -16,10 +30,19 @@ const RegisteredSemesters = () => {
       key: _id,
       name: academicSemester.name,
       status,
-      startDate,
-      endDate,
+      startDate: moment(new Date(startDate)).format('MMMM'),
+      endDate: moment(new Date(endDate)).format('MMMM'),
     })
   );
+
+  const handleStatusDropdown = (data: any) => {
+    console.log(data);
+  };
+
+  const menuProps = {
+    items,
+    onClick: handleStatusDropdown,
+  };
 
   const columns: TableColumnsType<TTbaleData> = [
     {
@@ -31,6 +54,19 @@ const RegisteredSemesters = () => {
       title: 'Status',
       dataIndex: 'status',
       key: 'status',
+      render: (item) => {
+        let color;
+        if (item === 'UPCOMING') {
+          color = 'blue';
+        }
+        if (item === 'ONGOING') {
+          color = 'green';
+        }
+        if (item === 'ENDED') {
+          color = 'red';
+        }
+        return <Tag color={color}>{item}</Tag>;
+      },
     },
     {
       title: 'Start Date',
@@ -48,7 +84,9 @@ const RegisteredSemesters = () => {
       render: () => {
         return (
           <div>
-            <Button>Update</Button>
+            <Dropdown menu={menuProps}>
+              <Button>Update</Button>
+            </Dropdown>
           </div>
         );
       },
