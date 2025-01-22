8cb996c3b244fd80afe03c5ec8ef046d93d6d4c3
sheabali <iamskpranto@gmail.com>
2025-01-22 16:48:40 +0600

    finsh

--- src/utils/sidebarItemGenerator.tsx
+++ src/utils/sidebarItemGenerator.tsx
@@ -1,7 +1,7 @@
 import { NavLink } from 'react-router-dom';
 import { TSidebarItem, TUserPaths } from '../types';
 
-export const sidebarItemsGenerator = (items: TUserPaths[], role) => {
+export const sidebarItemsGenerator = (items: TUserPaths[], role: string) => {
   const sidebarItem = items.reduce((acc: TSidebarItem[], item) => {
     if (item.path && item.name) {
       acc.push({
@@ -14,10 +14,16 @@ export const sidebarItemsGenerator = (items: TUserPaths[], role) => {
       acc.push({
         key: item.name,
         label: item.name,
-        children: item.children.map((child) => ({
-          key: child.name,
-          label: <NavLink to={`/${role}/${child.path}`}>{child.name}</NavLink>,
-        })),
+        children: item.children.map((child) => {
+          if (child.name) {
+            return {
+              key: child.name,
+              label: (
+                <NavLink to={`/${role}/${child.path}`}>{child.name}</NavLink>
+              ),
+            };
+          }
+        }),
       });
     }
 
